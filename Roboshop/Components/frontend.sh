4d24aad9ce12f2841005aba8662c06fd139c8a54
srikanthrk <srikanthrkdevops@gmail.com>
2022-03-13 20:15:21 +0530

    Adding a new sample for 10-03 for practice

--- Roboshop/Components/frontend.sh
+++ Roboshop/Components/frontend.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
